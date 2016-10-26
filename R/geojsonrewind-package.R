58d91201865ee8126d11e68a3f43e07a8fe2bde6
Scott Chamberlain <myrmecocystus@gmail.com>
2016-10-26 13:18:59 -0700

    add description field to pkg level man file, bumpd dev ver

--- R/geojsonrewind-package.R
+++ R/geojsonrewind-package.R
@@ -1,5 +1,8 @@
 #' @title \strong{GeoJSON Rewinding}
 #'
+#' @description a package for fixing ring order, see also the
+#' right-hand rule for geojson
+#'
 #' @name geojsonrewind-package
 #' @aliases geojsonrewind
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
