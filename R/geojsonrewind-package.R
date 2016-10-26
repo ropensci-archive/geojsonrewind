30fa9ecd921848552214a3c53d650ba55735c8fd
Scott Chamberlain <myrmecocystus@gmail.com>
2016-10-26 13:04:12 -0700

    added pkg level man file

---
+++ R/geojsonrewind-package.R
@@ -0,0 +1,7 @@
+#' @title \strong{GeoJSON Rewinding}
+#'
+#' @name geojsonrewind-package
+#' @aliases geojsonrewind
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @docType package
+NULL
